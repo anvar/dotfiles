71ddca1d111bd04c33646d99b79b4c5cc36f2d5d
Anvar Karimson <anvar@karimson.com>
2012-12-19 08:14:57 +0000

    adds init.el

---
+++ init.el
@@ -0,0 +1,19 @@
+;; User pack init file
+;;
+;; User this file to initiate the pack configuration.
+;; See README for more information.
+
+;; Load bindings config
+(live-load-config-file "bindings.el")
+(setq live-disable-zone t) 
+(setq-default indent-tabs-mode nil)
+
+(global-set-key "\C-x\C-m" 'execute-extended-command)
+(global-set-key "\C-c\C-m" 'execute-extended-command)
+
+(global-set-key "\C-w" 'backward-kill-word)
+(global-set-key "\C-x\C-k" 'kill-region)
+
+(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
